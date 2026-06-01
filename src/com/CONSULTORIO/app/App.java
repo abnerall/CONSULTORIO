43c7c424f258ddd40d9973b7452a69a81a018ca1
abner <julianapaza740@gmail.com>
2026-06-01 10:51:45 -0400

    feat:inicializando en github

--- src/com/CONSULTORIO/app/App.java
+++ src/com/CONSULTORIO/app/App.java
@@ -2,7 +2,7 @@ package com.CONSULTORIO.app; // <--- Esto le dice a Java exactamente dónde est�
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("=== SISTEMA CONSULTORIO MEDICO INICIADO ===");
+        System.out.println("=== SISTEMA CONSULTORIO MEDICO INICIADO === GITHUB VERIFICADO ");
         // Desde aquí orquestaremos el inicio del sistema
     }
 }
