e68ff29c2324eaf70e303ef6bc2a25f82b14ccaf
abner <julianapaza740@gmail.com>
2026-06-01 10:35:54 -0400

    feat: inicializar estructura de proyecto para mi consultorio medico

---
+++ src/com/CONSULTORIO/app/App.java
@@ -0,0 +1,8 @@
+package com.CONSULTORIO.app; // <--- Esto le dice a Java exactamente dónde está ubicado
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("=== SISTEMA CONSULTORIO MEDICO INICIADO ===");
+        // Desde aquí orquestaremos el inicio del sistema
+    }
+}
