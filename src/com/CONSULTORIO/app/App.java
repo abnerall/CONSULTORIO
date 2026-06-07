24a5d26f1316bf90616c1612ca892817dd3286e6
abner <julianapaza740@gmail.com>
2026-06-07 16:58:45 -0400

    delete: Prueba de conexion con github en Main.java

--- src/com/CONSULTORIO/app/App.java
+++
@@ -1,8 +0,0 @@
-package com.CONSULTORIO.app; // <--- Esto le dice a Java exactamente dónde está ubicado
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println("=== SISTEMA CONSULTORIO MEDICO INICIADO === GITHUB VERIFICADO ");
-        // Desde aquí orquestaremos el inicio del sistema
-    }
-}
